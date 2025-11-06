90b69b3ec88b4fe344f94d2c55d43efc4fad5f56
EndfinityDev <89363009+EndfinityDev@users.noreply.github.com>
2025-11-07 01:50:17 +0300

    Initial upload

---
+++ Assets/Editor/EasyCMS/ImportConfigs/ImportModelConfig.cs
@@ -0,0 +1,24 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+public class ImportModelConfig : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        var modelImporter = assetImporter as ModelImporter;
+        modelImporter.materialName = ModelImporterMaterialName.BasedOnMaterialName;
+        modelImporter.importAnimation = false;
+        modelImporter.addCollider = true;
+        modelImporter.animationType = ModelImporterAnimationType.None;
+        modelImporter.importBlendShapes = false;
+        modelImporter.importLights = false;
+        modelImporter.importVisibility = false;
+        modelImporter.importCameras = false;
+        modelImporter.normalCalculationMode = ModelImporterNormalCalculationMode.AreaAndAngleWeighted;
+        modelImporter.isReadable = true;
+        modelImporter.indexFormat = ModelImporterIndexFormat.Auto;
+        modelImporter.materialLocation = ModelImporterMaterialLocation.External;
+    }
+}
+#endif
